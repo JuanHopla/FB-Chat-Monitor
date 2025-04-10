ac3ce272f4c3e85cb8f1e7a6e4f9e8b703f0c8c2
Juan David Castrejon <juandavidcastrejonbermudes@gmail.com>
2025-04-10 07:01:38 -0500

    Added functionality to monitor and reply to messages in the Facebook Marketplace.

---
+++ tampermonkey-header.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         FB-Chat-Monitor
+// @namespace    https://github.com/JuanHopla/FB-Chat-Monitor
+// @version      0.1
+// @description  Extracts chat data from Messenger and Facebook Marketplace in real-time using MutationObserver.
+// @author       JuanHopla
+// @match        https://www.messenger.com/*
+// @match        https://www.facebook.com/marketplace/inbox*
+// @grant        none
+// @updateURL    https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
+// @downloadURL  https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
+// ==/UserScript==
