fe23b1463b17a2ac12a735031e72bd8c12515ce5
Juan David Castrejon <juandavidcastrejonbermudes@gmail.com>
2025-04-11 09:29:34 -0500

    Added build script for development version and upgrade Tampermonkey header to [DEV] with version 0.1-dev

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -1,12 +1,10 @@
 // ==UserScript==
-// @name         FB-Chat-Monitor
+// @name         FB-Chat-Monitor [DEV]
 // @namespace    https://github.com/JuanHopla/FB-Chat-Monitor
-// @version      0.1
+// @version      0.1-dev
 // @description  Extracts chat data from Messenger and Facebook Marketplace in real-time using MutationObserver.
 // @author       JuanHopla
 // @match        https://www.messenger.com/*
 // @match        https://www.facebook.com/marketplace/inbox*
 // @grant        none
-// @updateURL    https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
-// @downloadURL  https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
 // ==/UserScript==
