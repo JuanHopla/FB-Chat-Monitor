19f88a33538c0c4c7a0453f37d4931358a0ad842
Juan David Castrejon <juandavidcastrejonbermudes@gmail.com>
2025-04-28 16:13:16 -0500

    chore: update package.json for version 1.0.0 and improve scripts
    feat: remove aiIntegration.js as part of project restructuring
    
    fix: update tampermonkey header for version 1.0.0 and add necessary grants

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -1,10 +1,18 @@
 // ==UserScript==
-// @name         FB-Chat-Monitor [DEV]
+// @name         FB-Chat-Monitor
 // @namespace    https://github.com/JuanHopla/FB-Chat-Monitor
-// @version      0.1-dev
-// @description  Extracts chat data from Messenger and Facebook Marketplace in real-time using MutationObserver.
+// @version      1.0.0
+// @description  Monitor and auto-respond to Facebook Marketplace messages with AI assistance
 // @author       JuanHopla
 // @match        https://www.messenger.com/*
 // @match        https://www.facebook.com/marketplace/inbox*
-// @grant        none
+// @match        https://www.facebook.com/messages*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @connect      api.openai.com
+// @updateURL    https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
+// @downloadURL  https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
+// @license      MIT
+// @run-at       document-idle
 // ==/UserScript==
