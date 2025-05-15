03527690632ad614ff436c0c30c80847a16948b9
Juan David Castrejon <juandavidcastrejonbermudes@gmail.com>
2025-05-15 14:04:08 -0500

    Enhance Tampermonkey script metadata: added new match patterns for marketplace items, included additional grants for value management, and expanded connection permissions for Facebook and Messenger domains.

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -7,10 +7,15 @@
 // @match        https://www.messenger.com/*
 // @match        https://www.facebook.com/marketplace/inbox*
 // @match        https://www.facebook.com/messages*
+// @match        https://www.facebook.com/marketplace/item/*
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
+// @grant        GM_deleteValue
+// @grant        GM_listValues
 // @connect      api.openai.com
+// @connect      facebook.com
+// @connect      messenger.com
 // @updateURL    https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
 // @downloadURL  https://juanhopla.github.io/FB-Chat-Monitor/main.user.js
 // @license      MIT
