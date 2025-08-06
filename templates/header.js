69a45f959507e76d9266f9bcdeb89873b1c7d007
Juan David Castrejon <juandavidcastrejonbermudes@gmail.com>
2025-08-06 16:43:02 -0500

    chore: update version number to 1.2.0 in header files

--- templates/header.js
+++ templates/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FB-Chat-Monitor DEV
 // @namespace    https://github.com/JuanHopla/FB-Chat-Monitor
-// @version      1.1.0
+// @version      1.2.0
 // @description  Monitor and auto-respond to Facebook Marketplace messages with AI assistance
 // @author       JuanHopla
 // @match        https://www.messenger.com/*
